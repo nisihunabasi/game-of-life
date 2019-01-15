76be531109bd0ec390c121d2f514cf05bec28c26
nisihunabasi <nisihunabasi@gmail.com>
2019-01-15 18:27:08 +0900

    [add] 開始・終了できるようにした。ランダムだけど、セルが点滅するようになった。

--- js/main.js
+++ js/main.js
@@ -14,12 +14,36 @@ let constant = {
     height: 60,
     clippedWidth: 40,
     clippedHeight: 40,
-
+    preset: {
+        "puffer_train": [
+            [0, 0, 0, 1, 0,],
+            [0, 0, 0, 0, 1,],
+            [1, 0, 0, 0, 1,],
+            [0, 1, 1, 1, 1,],
+            [0, 0, 0, 0, 0,],
+            [0, 0, 0, 0, 0,],
+            [0, 0, 0, 0, 0,],
+            [1, 0, 0, 0, 0,],
+            [0, 1, 1, 0, 0,],
+            [0, 0, 1, 0, 0,],
+            [0, 0, 1, 0, 0,],
+            [0, 1, 0, 0, 0,],
+            [0, 0, 0, 0, 0,],
+            [0, 0, 0, 0, 0,],
+            [0, 0, 0, 1, 0,],
+            [0, 0, 0, 0, 1,],
+            [1, 0, 0, 0, 1,],
+            [0, 1, 1, 1, 1,],
+        ],
+    },
 };
 let state = {
     nowStage: [],
     nextStage: [],
-    origin: []
+    previousStage: [],
+    origin: [],
+    isStarted: true,
+    handleOfInterval: 0,
 };
 
 /* モデル */
@@ -31,6 +55,65 @@ class Model {
      */
     static generateEmptyStage(width, height) {
         return new Array(width * height);
+    }
+    /**
+     * 次世代を生成する。
+     * @param {*} stage 
+     * @param {*} width 
+     * @param {*} height 
+     * @return {Array}
+     */
+    static buildNextGeneration(stage, width, height) {
+        let result = [];
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                let nowIdx = Model.getOneDimIdxFrom(x, y, width);
+                result[nowIdx] = Model.whatComesNext(stage, nowIdx, width, height);
+            }
+        }
+
+        return result;
+    }
+    /**
+     * 対象セルの次の状態はどうなるのか返す。
+     * @param {*} stage 
+     * @param {*} idx 
+     * @param {*} width 
+     * @param {*} height 
+     * @return {int} 0=死亡 1=誕生or維持
+     */
+    static whatComesNext(stage, idx, width, height) {
+        return Math.round(Math.random());   //とりあえず適当に返す。
+    }
+    /**
+     * 全滅したか。
+     * @param {Array} stage 
+     */
+    static isDestroyed(stage) {
+        //生存(1)の要素が見つからなかったときにtrueを返す。
+        return (stage.indexOf(1) === -1);
+    }
+    /**
+     * 停滞しているか。
+     * @param {Array} stage 
+     * @param {Array} previousStage
+     */
+    static isStagnating(stage, previousStage) {
+        //この判別はめんどくさい・・・やめよっかな
+    }
+    
+    static getOneDimIdxFrom(x, y, width) {
+        return y * width + x;
+    }
+    static applyPreset(width, height, formation) {
+        let stage = Model.generateEmptyStage(width, height);
+        let formationWidth = formation[0].length;
+        let formationHeight = formation.length;
+        
+        //プリセットの初期配置を、ステージの中央に配置する。
+        //配置のための開始座標を決定する。
+        
+        
     }
 }
 
@@ -65,10 +148,11 @@ class Drawer {
         for (let y = startHeight; y < endHeight; y++) {
             let tmpElement = "<tr>";
             for (let x = startWidth; x < endWidth; x++) {
-                if (stage[x * y] === 1) {
-                    tmpElement += "<td class='stage__cell stage__cell--live'></td>";
+                let nowIdx = Model.getOneDimIdxFrom(x, y, width);
+                if (stage[nowIdx] === 1) {
+                    tmpElement += "<td class='stage__cell stage__cell--live' data-idx='" + nowIdx + "'></td>";
                 } else {
-                    tmpElement += "<td class='stage__cell stage__cell--dead'></td>";
+                    tmpElement += "<td class='stage__cell stage__cell--dead' data-idx='" + nowIdx + "'></td>";
                 }
                 
             }
@@ -85,7 +169,10 @@ function initialize() {
     //変数初期化、ステージ初期化
     state.nowStage = Model.generateEmptyStage(constant.width, constant.height);
     state.nextStage = [];
+    state.previousStage = [];
     state.origin = [];
+    state.isStarted = false;
+    state.handleOfInterval = 0;
     //描画する。実際のステージよりだいぶ小さめに描画するようにしたい。40*40くらいで。
     Drawer.buildAndDrawStage(state.nowStage, constant.width, constant.height, constant.clippedWidth, constant.clippedHeight);
 }
@@ -94,8 +181,49 @@ $(() => {
     //一番最初に初期化する。
     initialize();
     //まずはじめに、初期状態を決める。天地創造！！
-    //手で入れてもらう。入れ終わったら「Start」ボタンを押して始まる。
-    $("button#game-start").click((e) => {
-        Drawer.buildStage();
+
+    //セルをクリックすると、色がついたりつかなかったりする。それに合わせてstateの該当箇所も置き換える。
+    $("#stage")
+        .on("click", ".stage__cell--live", (e) => {
+            $(e.target).removeClass("stage__cell--live").addClass("stage__cell--dead");
+            state.nowStage[$(this).data("idx")] = 0;
+        })
+        .on("click", ".stage__cell--dead", (e) => {
+            $(e.target).removeClass("stage__cell--dead").addClass("stage__cell--live");
+            state.nowStage[$(this).data("idx")] = 1;
+        });
+    //プリセットのローディング。
+    $("#apply").on("click", (e) => {
+        let selectedPreset = $("#preset").val();
+        if (selectedPreset == "none") {
+            state.nowStage = Model.generateEmptyStage(constant.width, constant.height);
+        } else if (Object.keys(constant.preset).indexOf(selectedPreset) !== -1) {
+            
+        } else {
+            return;
+        }
+        Drawer.buildAndDrawStage(state.nowStage, constant.width, constant.height, constant.clippedWidth, constant.clippedHeight);
+    });
+    //手で初期状態を入れてもらう。入れ終わったら「Launch」ボタンを押して始まる。
+    $("button#launch").click((e) => {
+        if (state.isStarted) {
+            return false;
+        }
+        state.isStarted = true;
+        state.handleOfInterval = setInterval(() => {
+            state.previousStage = state.nowStage.slice(0);  //コピー
+            state.nowStage = Model.buildNextGeneration(state.nowStage, constant.width, constant.height);
+            Drawer.buildAndDrawStage(state.nowStage, constant.width, constant.height, constant.clippedWidth, constant.clippedHeight);
+        }, 500);
+
+        
+    });
+    //いつでもStopボタンで止められる。
+    $("button#stop").click((e) => {
+        if (!state.isStarted) {
+            return false;
+        }
+        state.isStarted = false;
+        clearInterval(state.handleOfInterval);
     });
 });
